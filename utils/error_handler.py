96d47fbc85531b1efb7af9d3b6a5d88d25705ce4
Martijn Katuin <martijnrichardk@gmail.com>
2018-09-09 22:12:56 +0200

    Eerste!

---
+++ utils/error_handler.py
@@ -0,0 +1,60 @@
+import traceback
+import sys
+from discord.ext import commands
+
+"""
+If you are not using this inside a cog, add the event decorator e.g:
+@bot.event
+async def on_command_error(ctx, error)
+For examples of cogs see:
+Rewrite:
+https://gist.github.com/EvieePy/d78c061a4798ae81be9825468fe146be
+Async:
+https://gist.github.com/leovoel/46cd89ed6a8f41fd09c5
+This example uses @rewrite version of the lib. For the async version of the lib, simply swap the places of ctx, and error.
+e.g: on_command_error(self, error, ctx)
+For a list of exceptions:
+http://discordpy.readthedocs.io/en/rewrite/ext/commands/api.html#errors
+"""
+
+
+class CommandErrorHandler:
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def on_command_error(self, ctx, error):
+        """The event triggered when an error is raised while invoking a command.
+        ctx   : Context
+        error : Exception"""
+
+        # This prevents any commands with local handlers being handled here in on_command_error.
+        if hasattr(ctx.command, 'on_error'):
+            return
+
+        ignored = (commands.CommandNotFound, commands.UserInputError)
+
+        # Allows us to check for original exceptions raised and sent to CommandInvokeError.
+        # If nothing is found. We keep the exception passed to on_command_error.
+        error = getattr(error, 'original', error)
+
+        # Anything in ignored will return and prevent anything happening.
+        if isinstance(error, ignored):
+            return
+
+        elif isinstance(error, commands.DisabledCommand):
+            return await ctx.send(f'{ctx.command} has been disabled.')
+
+        elif isinstance(error, commands.NoPrivateMessage):
+            try:
+                return await ctx.author.send(f'{ctx.command} can not be used in Private Messages.')
+            except:
+                pass
+
+        # For this error example we check to see where it came from...
+        elif isinstance(error, commands.BadArgument):
+            if ctx.command.qualified_name == 'tag list':  # Check if the command being invoked is 'tag list'
+                return await ctx.send('I could not find that member. Please try again.')
+
+        # All other Errors not returned come here... And we can just print the default TraceBack.
+        print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
+        traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
---
+++ utils/utilities.py
@@ -0,0 +1,22 @@
+import json
+
+
+class Utils:
+    def __init__(self, bot):
+        self.bot = bot
+        self.settings_file = 'data/settings.json'
+
+        self.settings = self.load_json(self.settings_file)
+
+    def save_json(self, filename, data):
+        with open(filename, encoding='utf-8', mode="w") as f:
+            json.dump(data, f, indent=4, sort_keys=True, separators=(',', ' : '))
+        return data
+
+    def load_json(self, filename):
+        with open(filename, encoding='utf-8', mode="r") as f:
+            data = json.load(f)
+        return data
+
+    async def _save_settings(self):
+        self.save_json(self.settings_file, self.settings)
