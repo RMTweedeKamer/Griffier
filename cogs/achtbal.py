78ac496d7ef431d2594b4c83308eecfd8da53abd
Martijn <PaddoInWonderland@users.noreply.github.com>
2018-11-01 19:14:33 +0100

    Update 20181101

---
+++ cogs/achtbal.py
@@ -0,0 +1,42 @@
+from discord.ext import commands
+from random import choice
+
+
+class Achtbal:
+    def __init__(self, bot, utils):
+        self.bot = bot
+        self.utils = utils
+        self.antwoorden = [
+            'Het is zeker.',
+            'Het is zo besloten.',
+            'Zonder enige twijfel.',
+            'Je kunt erop vertrouwen.',
+            'Volgens mij wel.',
+            'Zeer waarschijnlijk.',
+            'Goed vooruitzicht.',
+            'Vooruitzicht is wazig, probeer het opnieuw.',
+            'Vraag later opnieuw.',
+            'Dat kan ik je beter niet vertellen.',
+            'Momenteel niet te voorspellen.',
+            'Concentreer je en vraag opnieuw.',
+            'Reken er maar niet op.',
+            'Mijn antwoord is nee.',
+            'Mijn bronnen zeggen van niet.',
+            'Vooruitzicht is niet zo goed.',
+            'Zeer twijfelachtig.',
+            'Hahahaha! :joy:',
+            'Nee, gewoon nee.',
+            'Zou je dat wel willen?',
+            'Doe ff normaal.',
+            'Heb je nou niets beters te melden?',
+            'Ja, kek!',
+            'Misschien wel.',
+            'Vandaag heb je geluk.',
+            'God zegent U.',
+            'Dat kun je beter aan iemand anders vragen.'
+            ]
+
+    @commands.command(name='8ball', aliases=['8'])
+    async def eightball(self, context):
+        '''Voor als je er zelf niet meer uitkomt.'''
+        await context.send('**{}**'.format(choice(self.antwoorden)))
