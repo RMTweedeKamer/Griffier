b1a764b3b632d2d6b37477fb09dfc0794e3e5fd8
Martijn <PaddoInWonderland@users.noreply.github.com>
2019-01-11 17:36:09 +0100

    Bug fix for #4 where still only admin could mute

--- cogs/mute.py
+++ cogs/mute.py
@@ -8,16 +8,14 @@ class Mute:
         self.utils = utils
 
     @commands.command(name='mute', aliases=['zwijg'])
-    @commands.has_permissions(administrator=True)
-    @commands.has_role('Ondervoorzitter')
+    @commands.has_any_role('Secretaris-Generaal', 'Voorzitter', 'Ondervoorzitter')
     async def mute(self, context, member: discord.Member):
         '''Leg iemand het zwijgen op.'''
         await context.channel.set_permissions(member, send_messages=False)
         await context.message.add_reaction('\U0001F44B')
 
     @commands.command(name='unmute', aliases=['spreek'])
-    @commands.has_permissions(administrator=True)
-    @commands.has_role('Ondervoorzitter')
+    @commands.has_any_role('Secretaris-Generaal', 'Voorzitter', 'Ondervoorzitter')
     async def unmute(self, context, member: discord.Member):
         '''Iemand mag weer spreken'''
         await context.channel.set_permissions(member, send_messages=True)
