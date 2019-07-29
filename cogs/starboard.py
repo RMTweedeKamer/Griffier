76d7cc180311552762bac6279a4fc59c1162ca18
th8 <tspel@live.nl>
2019-07-29 23:03:50 +0200

    Added name to field

--- cogs/starboard.py
+++ cogs/starboard.py
@@ -74,7 +74,7 @@ class Starboard:
                 else:
                     embed = discord.Embed(color=discord.Color.blurple())
 
-                embed.add_field(name='', value='[Jump!]({})'.format(message.jump_url))
+                embed.add_field(name='Link', value='[Jump!]({})'.format(message.jump_url))
                 embed.set_author(name='📌 {0.display_name}#{0.discriminator} in {1.name}'.format(author, channel), icon_url=avatar)
 
                 if attachment:
